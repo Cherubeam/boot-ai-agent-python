197429778ce13ad69c7f488b08f9cde7205ed01b
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-11-28 16:24:39 +0100

    add get_file_content.py boilerplate

---
+++ functions/get_file_content.py
@@ -0,0 +1,4 @@
+def get_file_content(working_directory, file_path):
+  print(f"Getting content for file: {file_path} in working directory: {working_directory}")
+
+get_file_content("calculator", "main.py")

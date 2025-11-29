cf66c39ccc63ed416f1721d49c43c1bbed903db9
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-11-29 20:01:07 +0100

    add first validations for get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,19 @@
 def get_file_content(working_directory, file_path):
   print(f"Getting content for file: {file_path} in working directory: {working_directory}")
+  print(f"FILE PATH: {file_path}")
 
-get_file_content("calculator", "main.py")
+  try:
+    if file_path not in os.listdir(working_directory):
+      return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+  except Exception as e:
+    return f'Error: {e}'
+    
+  try:
+    is_file = os.path.isfile(file_path)
+  except Exception as e:
+    return f'Error: {e}'
+
+  if not is_file:
+    return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+get_file_content("calculator", "main")

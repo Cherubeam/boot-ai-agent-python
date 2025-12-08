9506e37b70aefaa390c3a535e2d6ad622cde5d79
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-12-08 17:05:42 +0100

    refactor get_file_content.py and add additional test cases in test_get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,43 +1,33 @@
 import os
 import sys
 from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent.parent))
 from config import MAX_CHARS
 
-def get_file_content(working_directory, file_path):
-  print(f"Getting content for file: {file_path} in working directory: {working_directory}")
-  print(f"FILE PATH: {file_path}")
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
-  print(f"LISTING WORKING DIRECTORY CONTENTS: {os.listdir(working_directory)}")
+def get_file_content(working_directory, file_path):
+  absolute_working_dir = os.path.abspath(working_directory)
+  target_dir = os.path.abspath(os.path.join(working_directory, file_path))
 
   try:
-    if file_path not in os.listdir(working_directory):
-      print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+    if not target_dir.startswith(absolute_working_dir):
       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
   except Exception as e:
-    print(f"Error accessing working directory: {e}")
     return f'Error: {e}'  
     
   try:
-    file_path = os.path.join(working_directory, file_path)
-    is_file = os.path.isfile(file_path)
-    print(f"IS FILE: {is_file}")
+    is_file = os.path.isfile(target_dir)
   except Exception as e:
     return f'Error: {e}'
 
   if not is_file:
-    print(f'Error: File not found or is not a regular file: "{file_path}"')
-    return f'Error: File not found or is not a regular file: "{file_path}"'
+    return f'Error: File not found or is not a regular file: "{target_dir}"'
 
   try:
-    with open(file_path, 'r') as f:
+    with open(target_dir, 'r') as f:
       file_content_string = f.read()
       if len(file_content_string) > MAX_CHARS:
-        print(f'{file_content_string[:10000]} [...File "{file_path}" truncated at {MAX_CHARS} characters]')
-        return f'{file_content_string[:10000]} [...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return f'{file_content_string[:10000]} [...File "{os.path.join(working_directory, file_path)}" truncated at {MAX_CHARS} characters]'
+      return file_content_string
   except Exception as e:
-    print(f"Error reading file: {e}")
     return f'Error: {e}'
-
-get_file_content("calculator", "main.py")
--- test_get_file_content.py
+++ test_get_file_content.py
@@ -7,8 +7,26 @@ class TestGetFilesContent(unittest.TestCase):
     result = get_file_content("calculator", "lorem.txt")
     self.assertTrue(result.startswith("Lorem Ipsum"))
     self.assertIn('[...File "calculator/lorem.txt" truncated at', result)
+
+  def test_file_content_current_directory(self):
+    result = get_file_content("calculator", "main.py")
+    self.assertIn("def main():", result)
     print(result)
 
+  def test_file_content_subdirectory(self):
+    result = get_file_content("calculator", "pkg/calculator.py")
+    self.assertIn("class Calculator:", result)
+    print(result)
+
+  def test_non_existent_directory(self):
+    result = get_file_content("calculator", "/bin/cat")
+    self.assertIn('Error: Cannot read "/bin/cat" as it is outside the permitted working directory', result)
+    print(result)
+
+  def test_non_existing_file(self):
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    self.assertIn('Error: File not found or is not a regular file:', result)
+    print(result)
 
 if __name__ == "__main__":
     unittest.main()

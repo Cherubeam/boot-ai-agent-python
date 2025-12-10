9aecd15362d8fba30936eda8635a41355e3567b3
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-12-10 10:09:40 +0100

    refactor get_file_content.py and get_files_info.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,33 +1,25 @@
 import os
 import sys
 from pathlib import Path
-from config import MAX_CHARS
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
   absolute_working_dir = os.path.abspath(working_directory)
-  target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+  absolute_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-  try:
-    if not target_dir.startswith(absolute_working_dir):
+  if not absolute_file_path.startswith(absolute_working_dir):
       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-  except Exception as e:
-    return f'Error: {e}'  
     
-  try:
-    is_file = os.path.isfile(target_dir)
-  except Exception as e:
-    return f'Error: {e}'
-
-  if not is_file:
-    return f'Error: File not found or is not a regular file: "{target_dir}"'
+  if not os.path.isfile(absolute_file_path):
+    return f'Error: File not found or is not a regular file: "{file_path}"'
 
   try:
-    with open(target_dir, 'r') as f:
+    with open(absolute_file_path, 'r') as f:
       file_content_string = f.read()
       if len(file_content_string) > MAX_CHARS:
         return f'{file_content_string[:10000]} [...File "{os.path.join(working_directory, file_path)}" truncated at {MAX_CHARS} characters]'
       return file_content_string
   except Exception as e:
-    return f'Error: {e}'
+    return f'Error reading file "{file_path}": {e}'
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,16 +3,21 @@ import os
 def get_files_info(working_directory, directory="."):
   absolute_working_dir = os.path.abspath(working_directory)
   target_dir = os.path.abspath(os.path.join(working_directory, directory))
+
   if not target_dir.startswith(absolute_working_dir):
     return f'Result for \'{directory}\' directory:\nError: Cannot list "{directory}" as it is outside the permitted working directory'
+  
   if not os.path.isdir(target_dir):
     return f'Error: "{directory}" is not a directory'
+  
   try:
     files_info = []
+
     if directory == ".":
       files_info.append("Result for current directory:")
     else:
       files_info.append(f'Result for \'{directory}\' directory:')
+      
     for filename in os.listdir(target_dir):
       filepath = os.path.join(target_dir, filename)
       file_size = 0
@@ -21,6 +26,7 @@ def get_files_info(working_directory, directory="."):
       files_info.append(
         f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
       )
+
     return "\n".join(files_info)
   except Exception as e:
     return f"Error listing files: {e}"

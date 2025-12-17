b33e1ff721c7a8a9345502da54b1c5a242808671
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-12-17 20:30:08 +0100

    update run_python_file.py

---
+++ functions/run_python_file.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    absolute_working_dir = os.path.abspath(working_directory)
+    absolute_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not absolute_file_path.startswith(absolute_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(absolute_file_path):
+        return f'Error: File "{file_path}" not found.'
+
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        completed_process = subprocess.run(
+            ["python", absolute_file_path] + args,
+            stdout=None,
+            stderr=None,
+            capture_output=True,
+            timeout=3000,
+        )
+
+        if completed_process.returncode != 0:
+            formatted_string = f"STDOUT: {completed_process.stdout.decode('utf-8')}\nSTDERR:Process exited with code {completed_process.returncode}"
+        elif completed_process.stdout == f'b""':
+            formatted_string = "No output produced"
+        else:
+            formatted_string = f"STDOUT: {completed_process.stdout.decode('utf-8')}\nSTDERR: {completed_process.stderr.decode('utf-8')}"
+    except Exception as e:
+        return f"Error: executing file: {e}"
+
+    return formatted_string
